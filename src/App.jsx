60444e09f28740197d71dd76c595436f9204406d
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-06-13 12:58:36 -0600

    feat: add React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './index.css'
+import Home from './pages/Home'
 
 function App() {
 
   return (
-    <>
+    // add BroswerRouter 
+    <BrowserRouter>
       
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+    
   )
 }
 
