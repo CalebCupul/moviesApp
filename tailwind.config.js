aa5e1e82b47f4ce7ce942496a7f2f5717fefcfd3
Caleb Cupul <caleb.cupul@alumnos.udg.mx>
2023-06-14 14:23:41 -0600

    feat: add custom fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+        'custom-gray': '#1F1F1F',
+        'custom-red': '#D10A27',
+        'custom-black': '#090909',
+    },
+    extend: {
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
